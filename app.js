c1fb7863b97fea690fc9c32acb5cb1cf874fcafa
duonghiep416 <duonghiep416@gmail.com>
2023-04-21 16:49:32 +0700

    Complete

--- app.js
+++ app.js
@@ -2,37 +2,74 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const itemBox = $$(".item-box");
 const fadeout = $(".box-hide.fade-out");
+const reset = $(".reset");
+const yourScore = $(".score span");
 let arrNumber = [1, 2, 3, 4, 5, 6, 7, 8];
-let randomArr = arrNumber.concat(arrNumber).sort(() => 0.5 - Math.random());
+let randomArr;
 let itemClicked = [];
+let numerClicked = 0;
+let score = 0;
+let isComparing = false;
+
+function random() {
+    randomArr = arrNumber.concat(arrNumber).sort(() => 0.5 - Math.random());
+}
+random();
 function assignItem() {
     itemBox.forEach(function (item, i) {
+        yourScore.innerHTML = score;
         const image = item.querySelector("img");
         const boxHide = item.querySelector(".box-hide");
         image.setAttribute("src", `./images/img-${randomArr[i]}.jpg`);
         item.onclick = function () {
-            if (boxHide.classList.contains("fade-out")) {
+            if (isComparing || boxHide.classList.contains("fade-out")) {
                 return; // already clicked
             }
             boxHide.classList.add("fade-out");
             itemClicked.push(item);
-            if (itemClicked.length === 2) {
-                // check if two items have been clicked
-                console.log(
-                    itemClicked[0].querySelector("img").getAttribute("src") ===
-                        itemClicked[1].querySelector("img").getAttribute("src")
-                );
-                setTimeout(() => {
-                    itemClicked.forEach((clickedItem) => {
-                        clickedItem
+            numerClicked++;
+            if (itemClicked.length % 2 === 0) {
+                if (
+                    itemClicked
+                        .at(-1)
+                        .querySelector("img")
+                        .getAttribute("src") !==
+                    itemClicked.at(-2).querySelector("img").getAttribute("src")
+                ) {
+                    isComparing = true;
+                    setTimeout(() => {
+                        itemClicked
+                            .at(-1)
                             .querySelector(".box-hide")
                             .classList.remove("fade-out");
-                    });
-                    itemClicked = [];
-                }, 800);
+                        itemClicked
+                            .at(-2)
+                            .querySelector(".box-hide")
+                            .classList.remove("fade-out");
+                        isComparing = false;
+                    }, 800);
+                } else {
+                    score += 10;
+                    yourScore.innerHTML = score;
+                }
             }
         };
     });
 }
 
 assignItem();
+
+function refresh() {
+    reset.onclick = function () {
+        score = 0;
+        random();
+        assignItem();
+
+        itemClicked.forEach(function (clickedItem) {
+            clickedItem.querySelector(".box-hide").classList.remove("fade-out");
+        });
+        itemClicked = [];
+    };
+}
+
+refresh();
